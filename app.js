0c6813dbe39a9c3007d8b7b55a64bd9c2a354201
Rup1n <145932902+Rup1n@users.noreply.github.com>
2023-12-10 12:14:10 -0600

    Added JS Files
    Added the files to the repository.

---
+++ app.js
@@ -0,0 +1,75 @@
+// let icecream = 10;
+// if(icecream = 10){
+// 	console.log("I want to buy an ice cream.");
+// }
+// else {
+// 	console.log("No, I dont need it.");
+// }
+
+// let icecream = prompt("Type a number");
+// if(icecream < 10){
+// 	console.log("I want a vanilla ice cream.")
+// } 
+// else if(icecream > 15){
+// 	console.log("I want a choclate ice cream.")
+// }
+// else{
+// 	console.log("I dont want an ice cream.")
+// }
+
+// for(var i = 25; i < 51; i++){
+// 	console.log(i);
+// }
+
+// for(var i = 0; i < 9; i = i+1){
+// 	console.log('Rupin')
+// 	console.log('Dipin')
+// }
+
+// function hello(){
+// 	console.log("Hello");
+// }
+// hello();
+// hello();
+
+// for(var i = 0; i < 101; i++){
+// 	hello();
+// }
+
+// function makenoodles(){
+// 	console.log("Buy noodles");
+// 	console.log("Boil water");
+// 	console.log("Make noodles");
+// 	console.log("Now sleep");
+// }
+// function makenoodles(){
+// 	console.log("Buy noodles");
+// 	console.log("Boil water");
+// 	console.log("Make noodles");
+// 	console.log("Now sleep");
+// }
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+// makenoodles();
+
+function ABCD(a){
+	console.log(a);
+}
+ABCD(12);
+ABCD(10);
